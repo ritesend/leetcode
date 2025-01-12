37c02896a22a9807e07b904faee2642dcd922231
Ritesen Dhar <101635863+ritesend@users.noreply.github.com>
2025-01-12 11:33:27 +0530

    Time: 8 ms (90.61%), Space: 30.2 MB (53.05%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n=s.size();
+        if(n%2!=0){
+            return false;
+        }
+
+        int open=0;
+        int close=0;
+        int unlocked1=0;
+        int unlocked2=0;
+
+        for(int i=0;i<n;i++){
+            if(locked[i]=='0'){
+                unlocked1++;
+            }
+            else if(s[i]=='('){
+                open++;
+            }
+            else if(s[i]==')'){
+                open--;
+            }
+            if(open+unlocked1<0){
+                return false;
+            }
+
+            int j=n-i-1;
+
+            if(locked[j]=='0'){
+                unlocked2++;
+            }
+            else if(s[j]==')'){
+                close++;
+            }
+            else if(s[j]=='('){
+                close--;
+            }
+            if(close+unlocked2<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};

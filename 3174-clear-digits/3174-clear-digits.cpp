0f1d5060170e2ee97e419c3aa3c77e4def32e49a
Ritesen Dhar <101635863+ritesend@users.noreply.github.com>
2025-02-10 10:52:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (70.42%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans="";
+
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
